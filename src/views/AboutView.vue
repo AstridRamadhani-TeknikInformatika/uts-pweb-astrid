3972c44f9a05ac6d15c1de4116d78292fe6209cc
AstridRamadhani-TeknikInformatika <12150121537@students.uin-suska.ac.id>
2023-05-15 21:39:50 +0700

    uts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-  </div>
+  <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/08/24/3667911957.jpg">
 </template>
+
+<style>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 100px;
+  font-size: 150px;
+}
+
+</style>
 
